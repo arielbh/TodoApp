f2aa875e8ecfa9c400b834157bc8b38c2f51048f
Ariel Ben Horesh <arielbh@gmail.com>
2014-03-23 20:36:22 +0200

    Simple todo creation

--- TodoApp/MainPage.xaml.cs
+++ TodoApp/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net;
 using System.Windows;
@@ -7,35 +8,32 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using TodoApp.Model;
 using TodoApp.Resources;
 
 namespace TodoApp
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        private ObservableCollection<Todo> _todo = new ObservableCollection<Todo>();
         // Constructor
         public MainPage()
         {
             InitializeComponent();
+            _todo.CollectionChanged += _todo_CollectionChanged;
+            DataContext = _todo;
 
-            // Sample code to localize the ApplicationBar
-            //BuildLocalizedApplicationBar();
         }
 
-        // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
+        void _todo_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            NoItemsText.Visibility = (_todo.Count> 0)? Visibility.Collapsed : Visibility.Visible;
+        }
 
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
-
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            _todo.Add(new Todo { Id = 1, Title = "fff"});
+            
+        }
     }
 }
---
+++ TodoApp/Model/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApp.Model
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
